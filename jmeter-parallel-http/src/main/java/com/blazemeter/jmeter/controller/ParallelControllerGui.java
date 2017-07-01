a082042a91b631f37792d921f8fb1b4046ab8e69
Andrey Pokhilko <apc4@ya.ru>
2017-07-01 10:52:50 -0700

    Add GUI label

--- jmeter-parallel-http/src/main/java/com/blazemeter/jmeter/controller/ParallelControllerGui.java
+++ jmeter-parallel-http/src/main/java/com/blazemeter/jmeter/controller/ParallelControllerGui.java
@@ -1,9 +1,20 @@
 package com.blazemeter.jmeter.controller;
 
-import org.apache.jmeter.control.gui.AbstractControllerGui;
+import org.apache.jmeter.control.gui.LogicControllerGui;
 import org.apache.jmeter.testelement.TestElement;
 
-public class ParallelControllerGui extends AbstractControllerGui {
+import java.awt.*;
+
+public class ParallelControllerGui extends LogicControllerGui {
+    private static final String MSG = "All direct child elements of this controller" +
+            " will be executed as parallel.";
+
+    public ParallelControllerGui() {
+        super();
+        Label lbl = new Label(MSG, Label.CENTER);
+        add(lbl);
+    }
+
     @Override
     public String getStaticLabel() {
         return "bzm - Parallel Controller";
