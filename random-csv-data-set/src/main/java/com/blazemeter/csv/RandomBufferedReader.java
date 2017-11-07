6aab6eb86bf662ffe42048cde01376129fcdff8c
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-07 17:06:01 +0300

    fix read multibytes char with RandomAccessFile

--- random-csv-data-set/src/main/java/com/blazemeter/csv/RandomBufferedReader.java
+++ random-csv-data-set/src/main/java/com/blazemeter/csv/RandomBufferedReader.java
@@ -1,9 +1,6 @@
 package com.blazemeter.csv;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.io.Reader;
+import java.io.*;
 
 public class RandomBufferedReader extends BufferedReader {
 
@@ -21,7 +18,20 @@ public class RandomBufferedReader extends BufferedReader {
 
     @Override
     public int read() throws IOException {
-        return raf.read();
+        for (;;) {
+            int b = raf.read();
+            // Single byte character starting with binary 0.
+            if ((b & 0x80) == 0)
+                return (char) b;
+            // 2-byte character starting with binary 110.
+            if ((b & 0xE0) == 0xC0)
+                return (char) ((b & 0x1F) << 6 | raf.read() & 0x3F);
+            // 3 and 4 byte encodings left as an exercise...
+            // 2nd, 3rd, or 4th byte of a multibyte char starting with 10.
+            // Back up and loop.
+            if ((b & 0xC0) == 0xF0)
+                raf.seek(raf.getFilePointer() - 2);
+        }
     }
 
     @Override
