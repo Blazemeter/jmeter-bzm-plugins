ab5116a47b50e3279f828e8a02dbc4b99e4f7a48
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-11-07 17:50:23 +0300

    fix random buffered reader

--- random-csv-data-set/src/main/java/com/blazemeter/csv/RandomBufferedReader.java
+++ random-csv-data-set/src/main/java/com/blazemeter/csv/RandomBufferedReader.java
@@ -18,19 +18,47 @@ public class RandomBufferedReader extends BufferedReader {
 
     @Override
     public int read() throws IOException {
-        for (;;) {
-            int b = raf.read();
-            // Single byte character starting with binary 0.
-            if ((b & 0x80) == 0)
-                return (char) b;
-            // 2-byte character starting with binary 110.
-            if ((b & 0xE0) == 0xC0)
-                return (char) ((b & 0x1F) << 6 | raf.read() & 0x3F);
-            // 3 and 4 byte encodings left as an exercise...
-            // 2nd, 3rd, or 4th byte of a multibyte char starting with 10.
-            // Back up and loop.
-            if ((b & 0xC0) == 0xF0)
-                raf.seek(raf.getFilePointer() - 2);
+        return readUtf8Char(raf);
+    }
+
+    public static int readUtf8Char(final DataInput dataInput) throws IOException {
+        int char1, char2, char3;
+
+        try {
+            char1 = dataInput.readByte() & 0xff;
+            switch (char1 >> 4) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+            /* 0xxxxxxx*/
+                    return (char) char1;
+                case 12:
+                case 13:
+            /* 110x xxxx   10xx xxxx*/
+                    char2 = dataInput.readByte() & 0xff;
+                    if ((char2 & 0xC0) != 0x80) {
+                        throw new UTFDataFormatException("malformed input");
+                    }
+                    return (char) (((char1 & 0x1F) << 6) | (char2 & 0x3F));
+                case 14:
+            /* 1110 xxxx  10xx xxxx  10xx xxxx */
+                    char2 = dataInput.readByte() & 0xff;
+                    char3 = dataInput.readByte() & 0xff;
+                    if (((char2 & 0xC0) != 0x80) || ((char3 & 0xC0) != 0x80)) {
+                        throw new UTFDataFormatException("malformed input");
+                    }
+                    return (char) (((char1 & 0x0F) << 12) | ((char2 & 0x3F) << 6) | ((char3 & 0x3F) << 0));
+                default:
+            /* 10xx xxxx,  1111 xxxx */
+                    throw new UTFDataFormatException("malformed input");
+            }
+        } catch (EOFException ex) {
+            return -1;
         }
     }
 
