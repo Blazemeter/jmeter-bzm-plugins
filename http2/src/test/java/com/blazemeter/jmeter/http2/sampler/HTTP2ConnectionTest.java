114f136458e0aae8ae40a8109537ecad06921dfe
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2018-02-09 19:22:05 -0300

    Add to Http2 plugin automatic await of responses at the beginning of each thread loop iteration to avoid running out of memory due to unbounded queuing of requests.
    Additionally change connection contract to reduce the number of methods and avoid time coupling between sync and getResults, and change the collection hosting the stream handlers to preserve order and improve synch logic performance and simplicity.

--- http2/src/test/java/com/blazemeter/jmeter/http2/sampler/HTTP2ConnectionTest.java
+++ http2/src/test/java/com/blazemeter/jmeter/http2/sampler/HTTP2ConnectionTest.java
@@ -52,19 +52,13 @@ public class HTTP2ConnectionTest {
     }
 
     @Test
-    public void addPendingResponsesTest() {
+    public void addPendingResponsesTest() throws InterruptedException {
         HTTP2SampleResult pendingResponse = new HTTP2SampleResult();
         pendingResponse.setPendingResponse(false);
         pendingResponse.setId(10);
         HTTP2StreamHandler streamHandler = new HTTP2StreamHandler(http2Connection, urlMock, headerManagerMock, cookieManagerMock, pendingResponse);
         http2Connection.addStreamHandler(streamHandler);
-        assertEquals(Collections.singletonList(pendingResponse), http2Connection.getResults());
-
-    }
-
-    @Test
-    public void syncTest() throws InterruptedException {
-        http2Connection.sync();
+        assertEquals(Collections.singletonList(pendingResponse), http2Connection.awaitResponses());
     }
 
 }
