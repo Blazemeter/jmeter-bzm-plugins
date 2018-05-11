88e68c748dfdab79920d69974f31702d0a517e43
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-05-11 15:59:10 +0300

    Fixed unnecessary TE clone

--- parallel/src/main/java/com/blazemeter/jmeter/controller/traverse/CustomTreeCloner.java
+++ parallel/src/main/java/com/blazemeter/jmeter/controller/traverse/CustomTreeCloner.java
@@ -1,9 +1,7 @@
 package com.blazemeter.jmeter.controller.traverse;
 
-import org.apache.jmeter.engine.util.NoThreadClone;
 import org.apache.jmeter.protocol.http.control.CookieManager;
 import org.apache.jmeter.protocol.http.control.ThreadSafeCookieManager;
-import org.apache.jmeter.testelement.TestElement;
 import org.apache.jorphan.collections.HashTree;
 import org.apache.jorphan.collections.HashTreeTraverser;
 import org.apache.jorphan.collections.ListedHashTree;
@@ -16,24 +14,8 @@ public class CustomTreeCloner implements HashTreeTraverser {
 
     private final LinkedList<Object> objects = new LinkedList<>();
 
-    private final boolean honourNoThreadClone;
-
-    /**
-     * Clone the test tree, honouring NoThreadClone markers.
-     *
-     */
     public CustomTreeCloner() {
-        this(true);
-    }
-
-    /**
-     * Clone the test tree.
-     *
-     * @param honourNoThreadClone set false to clone NoThreadClone nodes as well
-     */
-    public CustomTreeCloner(boolean honourNoThreadClone) {
         newTree = new ListedHashTree();
-        this.honourNoThreadClone = honourNoThreadClone;
     }
 
     /**
@@ -44,26 +26,10 @@ public class CustomTreeCloner implements HashTreeTraverser {
         if (node instanceof CookieManager) {
             node = new ThreadSafeCookieManager((CookieManager) node);
         }
-        node = addNodeToTree(node);
+        newTree.add(objects, node);
         addLast(node);
     }
 
-    /**
-     * @param node Node to add to tree or not
-     * @return Object node (clone or not)
-     */
-    protected Object addNodeToTree(Object node) {
-        if ((node instanceof TestElement) // Check can cast for clone
-                // Don't clone NoThreadClone unless honourNoThreadClone == false
-                && (!honourNoThreadClone || !(node instanceof NoThreadClone))
-                ) {
-            node = ((TestElement) node).clone();
-            newTree.add(objects, node);
-        } else {
-            newTree.add(objects, node);
-        }
-        return node;
-    }
 
     /**
      * add node to objects LinkedList
