5cfa8c0c6a470d6192bff08f73aa09285b4af887
Sebastián Lorenzo <sebastianlorenzo88@gmail.com>
2018-05-28 12:35:12 -0300

    removed RTE plugin from this branch

--- rte/src/main/java/com/blazemeter/jmeter/rte/core/RteIOException.java
+++
@@ -1,9 +0,0 @@
-package com.blazemeter.jmeter.rte.core;
-
-public class RteIOException extends Exception {
-
-  public RteIOException(Throwable cause) {
-    super("Communication error with RTE client", cause);
-  }
-
-}
--- rte/src/test/resources/jmeter/testJMeter.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-JMETER_VERSION=$1
-JMETER_PATH=${project.basedir}/.jmeter/$JMETER_VERSION
-
-mkdir -p $JMETER_PATH/lib/ext/ && cp -f rte/target/jmeter-test/lib/*.jar $JMETER_PATH/lib/ext/
-bzt -o modules.jmeter.path=$JMETER_PATH -o modules.jmeter.version=$JMETER_VERSION rte/target/jmeter-test/testJMeter.yaml
