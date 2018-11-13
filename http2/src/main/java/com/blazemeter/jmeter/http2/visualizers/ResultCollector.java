25436d692579b8e088e9550a9d12aec7f4bb774d
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-11-13 13:41:54 +0300

    move http2 plugin to own repo (#71)

--- http2/src/main/java/com/blazemeter/jmeter/http2/visualizers/ResultCollector.java
+++
@@ -1,9 +0,0 @@
-package com.blazemeter.jmeter.http2.visualizers;
-
-/**
- * Used to distinguish when the HTTP2Request has to send the HTTP2SampleResult without the
- * response.
- */
-public class ResultCollector extends org.apache.jmeter.reporters.ResultCollector {
-
-}
